acf61b7b3a761e26cbe537df38e5a2f02ca2742f
jia <jiakeith@gmail.com>
2017-06-29 15:41:50 -0700

    add /contractors api

---
+++ src/main/java/com/vDigit/rpm/web/ContractorsController.java
@@ -0,0 +1,27 @@
+package com.vDigit.rpm.web;
+
+import com.vDigit.rpm.dto.Contractor;
+import com.vDigit.rpm.dto.Contractors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/")
+public class ContractorsController {
+    private Contractors contractors = new Contractors();
+
+	@RequestMapping(value = "/contractors", method = RequestMethod.GET)
+	public @ResponseBody
+    Collection<Contractor> getContractors() {
+        return contractors.getContractors();
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "Test..";
+	}
+}
