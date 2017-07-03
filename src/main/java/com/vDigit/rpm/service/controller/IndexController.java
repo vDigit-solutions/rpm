68c5053cbd2bf8f5519907a57fffc5eca9126062
Reddy, Ramesh (DS) <Ramesh.Reddy@cdk.com>
2017-07-02 19:32:05 -0700

    changed the method name...

--- src/main/java/com/vDigit/rpm/service/controller/IndexController.java
+++ src/main/java/com/vDigit/rpm/service/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	@RequestMapping("/")
-	public String index() {
+	public String jobRequest() {
 		return "jobRequest";
 	}
 }
