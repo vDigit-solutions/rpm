8bdf4b573ad6c3e8da3a45e5f49fb2ee63077a82
jia <jiakeith@gmail.com>
2017-07-31 18:05:42 -0700

    integrate angular 2 setup

---
+++ src/main/webapp/quickstart/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from "./mock-heroes";
+import {Hero} from "./hero";
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(HEROES), 500);
+    });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
+}
