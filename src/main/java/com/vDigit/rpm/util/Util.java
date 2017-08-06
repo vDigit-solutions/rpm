7e7d435201cdf62cbfb8f27034493f96a04ac86f
Siva Dosapati <siva.dosapati@gmail.com>
2017-08-05 21:37:50 -0700

    Test

--- src/main/java/com/vDigit/rpm/util/Util.java
+++ src/main/java/com/vDigit/rpm/util/Util.java
@@ -3,6 +3,8 @@ package com.vDigit.rpm.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Util {
+
+	// Test
 	private static ObjectMapper objectMapper = new ObjectMapper();
 
 	public static String toJSON(Object object) {
